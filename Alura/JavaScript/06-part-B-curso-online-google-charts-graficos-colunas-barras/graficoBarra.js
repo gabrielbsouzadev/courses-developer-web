0f336df3c92c8bf62e2a45eed7afdd28e68ffaaf
Gabriel B Souza <60492389+gabrielbsouzadev@users.noreply.github.com>
2020-09-12 19:55:33 -0300

    Conhecendo os tipos de empilhamento

--- Alura/JavaScript/06-part-B-curso-online-google-charts-graficos-colunas-barras/graficoBarra.js
+++ Alura/JavaScript/06-part-B-curso-online-google-charts-graficos-colunas-barras/graficoBarra.js
@@ -1,72 +1,119 @@
 function desenharGraficoPizza() {
 
-    var tabela = new google.visualization.DataTable();
+    // var tabela = new google.visualization.DataTable();
 
-    tabela.addColumn('string', 'Categorias');
-    tabela.addColumn('number', 'Tecnologia');
-    tabela.addColumn({
-        type: 'number',
-        role: 'annotation'
-    });
-    tabela.addColumn({
-        type: 'string',
-        role: 'style'
-    });
-    tabela.addColumn('number', 'Marketing');
-    tabela.addColumn({
-        type: 'number',
-        role: 'annotation'
-    });
-    tabela.addColumn({
-        type: 'string',
-        role: 'style'
-    });
-    tabela.addColumn('number', 'Administrativo');
-    tabela.addColumn({
-        type: 'number',
-        role: 'annotation'
-    });
-    tabela.addColumn({
-        type: 'string',
-        role: 'style'
-    });
-    tabela.addColumn('number', 'Linhas de crédito');
-    tabela.addColumn({
-        type: 'number',
-        role: 'annotation'
-    });
-    tabela.addColumn({
-        type: 'string',
-        role: 'style'
-    });
-    tabela.addColumn('number', 'Segurança');
-    tabela.addColumn({
-        type: 'number',
-        role: 'annotation'
-    });
-    tabela.addColumn({
-        type: 'string',
-        role: 'style'
-    });
-    tabela.addColumn('number', 'Atendimento ao cliente');
-    tabela.addColumn({
-        type: 'number',
-        role: 'annotation'
-    });
-    tabela.addColumn({
-        type: 'string',
-        role: 'style'
-    });
+    // tabela.addColumn('string', 'Categorias');
+    // tabela.addColumn('number', 'Tecnologia');
+    // tabela.addColumn({
+    //     type: 'number',
+    //     role: 'annotation'
+    // });
+    // tabela.addColumn({
+    //     type: 'string',
+    //     role: 'style'
+    // });
+    // tabela.addColumn('number', 'Marketing');
+    // tabela.addColumn({
+    //     type: 'number',
+    //     role: 'annotation'
+    // });
+    // tabela.addColumn({
+    //     type: 'string',
+    //     role: 'style'
+    // });
+    // tabela.addColumn('number', 'Administrativo');
+    // tabela.addColumn({
+    //     type: 'number',
+    //     role: 'annotation'
+    // });
+    // tabela.addColumn({
+    //     type: 'string',
+    //     role: 'style'
+    // });
+    // tabela.addColumn('number', 'Linhas de crédito');
+    // tabela.addColumn({
+    //     type: 'number',
+    //     role: 'annotation'
+    // });
+    // tabela.addColumn({
+    //     type: 'string',
+    //     role: 'style'
+    // });
+    // tabela.addColumn('number', 'Segurança');
+    // tabela.addColumn({
+    //     type: 'number',
+    //     role: 'annotation'
+    // });
+    // tabela.addColumn({
+    //     type: 'string',
+    //     role: 'style'
+    // });
+    // tabela.addColumn('number', 'Atendimento ao cliente');
+    // tabela.addColumn({
+    //     type: 'number',
+    //     role: 'annotation'
+    // });
+    // tabela.addColumn({
+    //     type: 'string',
+    //     role: 'style'
+    // });
 
-    tabela.addRows([
-        //   ['Tecnologia', 47814, 47814, 12345, 'color:#a6a6a6', 12345],
-        //   ['Marketing', 22552, 22552, 6789, 'color:#a6a6a6', 6789],
-        //   ['Administrativo', 65243, 'color:#1b9af5', 65243],
-        //   ['Linhas de crédito', 55504, 'color:#a6a6a6', 55504],
-        //   ['Segurança', 86085, 'color:#f5921b', 86085],
-        //   ['Atendimento ao cliente', 12605, 'color:#a6a6a6', 12605]
+    // tabela.addRows([
+    //     //   ['Tecnologia', 47814, 47814, 12345, 'color:#a6a6a6', 12345],
+    //     //   ['Marketing', 22552, 22552, 6789, 'color:#a6a6a6', 6789],
+    //     //   ['Administrativo', 65243, 'color:#1b9af5', 65243],
+    //     //   ['Linhas de crédito', 55504, 'color:#a6a6a6', 55504],
+    //     //   ['Segurança', 86085, 'color:#f5921b', 86085],
+    //     //   ['Atendimento ao cliente', 12605, 'color:#a6a6a6', 12605]
 
-        ['Categorias',
+    //     [
+    //         'Categorias',
+    //         47814, 47814, 'color:#a6a6a6',
+    //         22552, 22552, 'color:#a6a6a6',
+    //         65243, 65243, 'color:#1b9af5',
+    //         55504, 55504, 'color:#a6a6a6',
+    //         86085, 86085, 'color:#f5921b',
+    //         12605, 12605, 'color:#a6a6a6'
+    //     ]
+    // ]);
+
+    var tabela = new google.visualization.arrayToDataTable([
+
+        [
+            'Categorias',
+            'Tecnologia', {
+                role: 'annotation'
+            }, {
+                role: 'style'
+            },
+            'Marketing', {
+                role: 'annotation'
+            }, {
+                role: 'style'
+            },
+            'Administrativo', {
+                role: 'annotation'
+            }, {
+                role: 'style'
+            },
+            'Linhas de crédito', {
+                role: 'annotation'
+            }, {
+                role: 'style'
+            },
+            'Segurança', {
+                role: 'annotation'
+            }, {
+                role: 'style'
+            },
+            'Atendimento ao cliente', {
+                role: 'annotation'
+            }, {
+                role: 'style'
+            }
+        ],
+        [
+            'Categorias', 
             47814, 47814, 'color:#a6a6a6',
             22552, 22552, 'color:#a6a6a6',
             65243, 65243, 'color:#1b9af5',
@@ -74,12 +121,13 @@ function desenharGraficoPizza() {
             86085, 86085, 'color:#f5921b',
             12605, 12605, 'color:#a6a6a6'
         ]
+
     ]);
 
-    tabela.sort({
-        column: 1,
-        asc: true
-    });
+    // tabela.sort({
+    //     column: 1,
+    //     asc: true
+    // });
 
     var opcoes = {
         title: 'Gastos por unidade de negócio',
@@ -87,7 +135,7 @@ function desenharGraficoPizza() {
         width: 1000,
         legend: 'none',
         hAxis: {
-            textPosition: 'none',
+            // textPosition: 'none',
         },
         isStacked: true,
         //pieSliceText: 'value',
