25c6e279ec4cce2a54e65c1dc197f16473c2b8b4
Gabriel Souza <gabrielbsouzadev@gmail.com>
2020-09-14 04:14:58 -0300

    07-expressoes_regulares-Capturando textos de forma_magica - start

--- Alura/JavaScript/03-javascript-programando-na-linguagem-web/introducao-javascript/js/buscar-pacientes.js
+++ Alura/JavaScript/03-javascript-programando-na-linguagem-web/introducao-javascript/js/buscar-pacientes.js
@@ -11,8 +11,10 @@ var botaoAdicionar = document.querySelector("#buscar-pacientes");
 botaoAdicionar.addEventListener("click", function() {
     
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
+    // Buscando da URL abaixo como exemplo da Alura
+    // xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
 
+    xhr.open("GET", "https://gist.githubusercontent.com/gabrielbsouzadev/de3a28c527f308726b2f4a90286d549b/raw/9b3ab3f5564bd874900519b0db48755f042c189a/pacientes.json");
 
     xhr.addEventListener("load", function() {
         var erroAjax = document.querySelector("#erro-ajax");
