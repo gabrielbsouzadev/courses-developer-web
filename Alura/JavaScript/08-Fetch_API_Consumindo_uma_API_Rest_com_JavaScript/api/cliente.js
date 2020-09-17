2d4200ab32323c6afaa7730bab532a133132784a
Gabriel B Souza <60492389+gabrielbsouzadev@users.noreply.github.com>
2020-09-17 16:49:53 -0300

    Criando o cliente - finish

--- Alura/JavaScript/08-Fetch_API_Consumindo_uma_API_Rest_com_JavaScript/api/cliente.js
+++ Alura/JavaScript/08-Fetch_API_Consumindo_uma_API_Rest_com_JavaScript/api/cliente.js
@@ -1,4 +1,4 @@
-listarClientes = () => {
+const listarClientes = () => {
 
     return fetch('http://localhost:4000/clientes')
     .then(resposta => {
@@ -8,4 +8,21 @@ listarClientes = () => {
         return json
     })
 
+}
+
+const cadastrarClientes = (nome, cpf) => {
+    const json = JSON.stringify({
+        nome: nome,
+        cpf: cpf
+    })
+    return fetch('http://localhost:4000/clientes/cliente', {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: json
+    })
+    .then( resp => {
+        return resp.body
+    })
 }
