101f970d87733ee0a1f249ac9698b61839e09c4b
Gabriel B Souza <60492389+gabrielbsouzadev@users.noreply.github.com>
2020-09-17 17:52:49 -0300

    Criando o algoritmo de validação - finish

---
+++ Alura/JavaScript/08-Fetch_API_Consumindo_uma_API_Rest_com_JavaScript/validaCPF.js
@@ -0,0 +1,29 @@
+function verificaCPF(cpf) {
+    const cpfInvalidos = [
+        "11111111111",
+        "22222222222",
+        "33333333333",
+        "44444444444",
+        "55555555555",
+        "66666666666",
+        "77777777777",
+        "88888888888",
+        "99999999999",
+        "00000000000"
+    ]
+
+    return cpfInvalidos.indexOf(cpf) === -1
+
+}
+
+function somaNumerosCPF(cpf, totalDeDigitos, peso){
+
+    let soma = 0
+
+    for (let indice = 1; indice <= totalDeDigitos; indice++) {
+        soma += parseInt(cpf.substring(indice - 1, indice)) * ( peso  - indice)
+    }
+
+    return soma
+    
+}
