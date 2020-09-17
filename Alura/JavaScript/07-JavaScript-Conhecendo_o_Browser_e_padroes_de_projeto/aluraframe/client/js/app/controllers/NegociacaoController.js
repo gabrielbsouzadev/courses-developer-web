69cdf689e0b919010fdc27f7006ffdebde56055a
Gabriel_Souza <gabrielbsouzadev@gmail.com>
2020-09-17 14:47:05 -0300

    Associando métodos do controller às ações do usuário - start

---
+++ Alura/JavaScript/07-JavaScript-Conhecendo_o_Browser_e_padroes_de_projeto/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,16 @@
+// class NegociacaoController {
+
+//     adiciona(event){
+//         event.preventDefault();
+//         alert("Chamei adiciona no constructor");
+//     }
+
+// }
+
+class NegociacaoController {
+
+    adiciona(event) {
+          event.preventDefault();
+          alert('Chamei ação no controller');
+    }
+  }
