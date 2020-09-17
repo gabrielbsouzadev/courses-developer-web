802d0501da4498fcb733c19bae38bffde8ee0293
Gabriel_Souza <gabrielbsouzadev@gmail.com>
2020-09-17 20:05:51 -0300

    Template Strings - finish

--- Alura/JavaScript/07-JavaScript-Conhecendo_o_Browser_e_padroes_de_projeto/aluraframe/client/js/app/helpers/dateHelper.js
+++ Alura/JavaScript/07-JavaScript-Conhecendo_o_Browser_e_padroes_de_projeto/aluraframe/client/js/app/helpers/dateHelper.js
@@ -1,15 +1,24 @@
 class DateHelper {
 
-    dataParaTexto(data) {
+    constructor() {
 
-        return data.getDate()
-            + '/' + (data.getMonth() + 1)
-            + '/' + data.getFullYear();
-      }
+        throw new Error('Esta classe não pode ser instanciada');
 
-    textoParaData(texto) {
+    }
 
-        return new Date(...texto.split('-').map((item,indice) => item - indice % 2));
+    static dataParaTexto(data) {
+
+        return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
+
+    }
+
+    static textoParaData(texto) {
+
+        if (!/^\d{4}-\d{2}-\d{2}$/.test(texto))
+
+            throw new Error('Deve estar no formato aaaa-mm-dd');
+
+            return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
 
     }
 }
