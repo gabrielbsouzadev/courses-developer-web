dadb6ad5d7666356eeed285ffff87299d8b497c7
Gabriel Souza <gabrielbsouzadev@gmail.com>
2020-09-21 09:32:50 -0300

    Hora de excluir um cliente - finish

--- Alura/JavaScript/08-Fetch_API_Consumindo_uma_API_Rest_com_JavaScript/listagem-cliente.js
+++ Alura/JavaScript/08-Fetch_API_Consumindo_uma_API_Rest_com_JavaScript/listagem-cliente.js
@@ -1,8 +1,12 @@
-
+const removeCliente = id => {
+    if (confirm("Deseja deletar o cliente?")) {
+        deletaCliente(id)
+    }
+}
 
 const corpoTabela = document.querySelector("[data-conteudo-tabela]")
 
-const exibeCliente = (cpf, nome) => {
+const exibeCliente = (cpf, nome, id) => {
 
     const linha = document.createElement('tr')
 
@@ -10,6 +14,7 @@ const exibeCliente = (cpf, nome) => {
     `
         <td>${cpf}</td>
         <td>${nome}</td>
+        <button type="button" class="btn btn-danger" onclick="removeCliente(${id})">Excluir</button>
     `
 
     linha.innerHTML = conteudoLinha
@@ -19,6 +24,6 @@ const exibeCliente = (cpf, nome) => {
 
 listarClientes().then( exibe => {
     exibe.forEach(indice => {
-        corpoTabela.appendChild(exibeCliente(indice.cpf, indice.nome))
+        corpoTabela.appendChild(exibeCliente(indice.cpf, indice.nome, indice.id))
     });
 })
