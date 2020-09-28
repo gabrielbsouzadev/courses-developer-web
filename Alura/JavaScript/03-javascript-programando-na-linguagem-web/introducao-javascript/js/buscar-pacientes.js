271bb439cf8b59efad8f13badb3cdaa2f9155100
Gabriel_Souza <gabrielbsouzadev@gmail.com>
2020-09-28 20:16:50 -0300

    Update Load list pacients on up page - 03-javascript-programando-na-linguagem-web

--- Alura/JavaScript/03-javascript-programando-na-linguagem-web/introducao-javascript/js/buscar-pacientes.js
+++ Alura/JavaScript/03-javascript-programando-na-linguagem-web/introducao-javascript/js/buscar-pacientes.js
@@ -1,11 +1,37 @@
-//implementando buscar pacientes ao carregar a pagina
+
+// //implementando buscar pacientes ao carregar a pagina
 // window.onload = () => {
 //     var xhr = new XMLHttpRequest();
-//     xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
+//     // xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
+
+//     // xhr.send();
+
+//     //var xhr = new XMLHttpRequest();
+//     // Buscando da URL abaixo como exemplo da Alura
+//     // xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
+
+//     xhr.open("GET", "https://gist.githubusercontent.com/gabrielbsouzadev/de3a28c527f308726b2f4a90286d549b/raw/9b3ab3f5564bd874900519b0db48755f042c189a/pacientes.json");
+
+//     xhr.addEventListener("load", function() {
+//         var erroAjax = document.querySelector("#erro-ajax");
+
+//         if (xhr.status == 200) {
+//             erroAjax.classList.add("invisivel");
+//             var resposta = xhr.responseText;
+//             var pacientes = JSON.parse(resposta);
+
+//             pacientes.forEach(function(paciente) {
+//                 adicionaPacienteNaTabela(paciente);
+//             });
+//         } else {
+//             erroAjax.classList.remove("invisivel");
+//         }
+//     });
 
 //     xhr.send();
 // }
 
+/* Buscar lista pelo botão*/
 var botaoAdicionar = document.querySelector("#buscar-pacientes");
 
 botaoAdicionar.addEventListener("click", function() {
