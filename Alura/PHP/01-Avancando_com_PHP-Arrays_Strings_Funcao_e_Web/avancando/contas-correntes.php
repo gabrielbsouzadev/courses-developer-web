f51655b3f7a55f8278c08ef99f1d50aa1d2a3bf2
Gabriel_Souza <gabrielbsouzadev@gmail.com>
2020-10-13 23:57:00 -0300

    Arrays assosciativos

--- Alura/PHP/01-Avancando_com_PHP-Arrays_Strings_Funcao_e_Web/avancando/contas-correntes.php
+++ Alura/PHP/01-Avancando_com_PHP-Arrays_Strings_Funcao_e_Web/avancando/contas-correntes.php
@@ -1,11 +1,48 @@
 <?php
 
-$conta1 = array('titular' => "Vinicius", 'saldo' => 1000);
-$conta2 = array('titular' => "Maria", 'saldo' => 10000);
-$conta3 = array('titular' => "Alberto", 'saldo' => 300);
+// $conta1 = array('titular' => "Vinicius", 'saldo' => 1000);
+// $conta2 = array('titular' => "Maria", 'saldo' => 10000);
+// $conta3 = array('titular' => "Alberto", 'saldo' => 300);
 
-$contaCorrentes = [$conta1, $conta2, $conta3];
+// $contaCorrentes = [$conta1, $conta2, $conta3];
 
-for ($i=0; $i < count($contaCorrentes); $i++) { 
-    echo ($contaCorrentes[$i]['titular'] . "<br>");
+// for ($i=0; $i < count($contaCorrentes); $i++) { 
+//     echo ($contaCorrentes[$i]['titular'] . "<br>");
+// }
+
+$contasCorrentes = [
+    '123.456.789-10' => [
+        'titular' => 'Maria',
+        'saldo' => 10000
+    ],
+    '123.345.689-11' => [
+        'titular' => 'Alberto',
+        'saldo' => 300
+    ],
+    '123.256.789-12' => [
+        'titular' => 'Vinicius',
+        'saldo' => 100 
+    ]
+];
+
+foreach ($contasCorrentes as $cpf => $conta) {
+    echo($cpf . " " . $conta['titular'] . "<br>");
 }
+
+// echo ("Carros: <br>");
+
+// //Exemplos para Carros
+// $carros = [
+//     'FLN-3911' => [
+//         'marca' => 'Honda',
+//         'modelo' => 'Civic'
+//     ],
+//     'CSP-6752' => [
+//         'marca' => 'VW',
+//         'modelo' => 'Brasilia'
+//     ]  
+// ];
+
+// foreach ($carros as $placa => $carro) {
+//     echo($placa . ": " . $carro["marca"] . "<br>");
+// }
