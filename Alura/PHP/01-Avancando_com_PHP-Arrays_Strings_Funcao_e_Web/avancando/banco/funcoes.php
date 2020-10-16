515f764c289c8af94b96ab8e77643b48617a1e3c
Gabriel B Souza <60492389+gabrielbsouzadev@users.noreply.github.com>
2020-10-16 00:56:12 -0300

    Vriavel por referencia

---
+++ Alura/PHP/01-Avancando_com_PHP-Arrays_Strings_Funcao_e_Web/avancando/banco/funcoes.php
@@ -0,0 +1,36 @@
+<?php
+
+function exibeMensagem(string $mensagem)
+{
+    echo "$mensagem <br>";
+}
+
+function depositar(array $conta, float $valorADepositar): array
+{
+    if ($valorADepositar > 0) {
+        $conta['saldo'] += $valorADepositar;
+    } else {
+        exibeMensagem("Depositos precisam ser positivos");
+    }
+    return $conta;
+}
+
+function sacar(array $conta, float $valorASacar): array
+{
+    if ($valorASacar > $conta['saldo']) {
+        exibeMensagem("Você não tem saldo suficiente");
+    } else {
+        $conta['saldo'] -= $valorASacar;
+    }
+
+    return $conta;
+}
+
+function titularComLetrasMaiuscular(array &$conta){
+    echo ($conta['titular']);
+    $conta['titular'] = strtoupper($conta['titular']);
+}
+
+foreach ($contasCorrentes as $cpf => $conta) {
+    exibeMensagem($cpf . " " . $conta['titular'] . ' ' . $conta['saldo']);
+}
