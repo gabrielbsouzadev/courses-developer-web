9ce558da59b7aac4d33b8a6b13ba65593678c590
gabrielbsouzadev <gabrielbsouzadev@gmail.com>
2020-10-21 07:47:14 -0300

    redirect - ok - Utilizando o Header Location

--- Alura/PHP/04-PHP_e_MySQL-Introducao_a_uma_webapp/admin/adicionar-artigo.php
+++ Alura/PHP/04-PHP_e_MySQL-Introducao_a_uma_webapp/admin/adicionar-artigo.php
@@ -8,7 +8,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $artigo = new Artigo($mysql);
     $artigo->adicionar($_POST['titulo'], $_POST['conteudo']);
 
+    //redirecionar
+    header('Location: adicionar-artigo.php');
+    die();
 }
+
+
 
 ?>
 
