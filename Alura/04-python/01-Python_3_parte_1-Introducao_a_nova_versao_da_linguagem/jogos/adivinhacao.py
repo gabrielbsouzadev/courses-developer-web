7cd67a682f2b0d2c0f867bcba75d4492448392db
gabrielbsouzadev <gabrielbsouzadev@gmail.com>
2020-11-22 11:38:05 -0300

    Condicao While - entendida

--- Alura/04-python/01-Python_3_parte_1-Introducao_a_nova_versao_da_linguagem/jogos/adivinhacao.py
+++ Alura/04-python/01-Python_3_parte_1-Introducao_a_nova_versao_da_linguagem/jogos/adivinhacao.py
@@ -3,31 +3,39 @@ print("bem vindo ao jogo de adivinhacao")
 print("********************************")
 
 numero_secreto = 43;
+total_de_tentativas = 3;
+rodada = 1
 
-chute_str = input("Digite seu numero: ")
-chute_int = int(chute_str)
+while( rodada <= total_de_tentativas ):
+    print("tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute_str = input("Digite seu numero: ")
+    chute_int = int(chute_str)
 
-print("Voce digitou ", chute_int)
+    print("Voce digitou ", chute_int)
 
-acertou = numero_secreto == chute_int
-maior = chute_int > numero_secreto
-menor = chute_int < numero_secreto
+    acertou = numero_secreto == chute_int
+    maior = chute_int > numero_secreto
+    menor = chute_int < numero_secreto
 
-if (acertou):
-    print("Você acertou")
-else:
-    if (maior):
-        print("Você errou! Seu chute foi maior que o número secreto")
-    #Ou poderia usar o else     
-    elif(menor):
-        print("Você errou! Seu chute foi menor que o número secreto")
+    if (acertou):
+        print("Você acertou")
+        break;
+    else:
+        if (maior):
+            print("Você errou! Seu chute foi maior que o número secreto")
+        #Ou poderia usar o else     
+        elif(menor):
+            print("Você errou! Seu chute foi menor que o número secreto")
 
-print("********************************")
-print("Comparando a idade do casal")
-print("********************************")
-minha_idade = 25
-idade_namorada = 19
-if(minha_idade == idade_namorada):
-    print('temos idades iguais')
-else:
-    print('temos idades diferentes')
+    rodada = rodada + 1
+
+
+# print("********************************")
+# print("Comparando a idade do casal")
+# print("********************************")
+# minha_idade = 25
+# idade_namorada = 19
+# if(minha_idade == idade_namorada):
+#     print('temos idades iguais')
+# else:
+#     print('temos idades diferentes')
