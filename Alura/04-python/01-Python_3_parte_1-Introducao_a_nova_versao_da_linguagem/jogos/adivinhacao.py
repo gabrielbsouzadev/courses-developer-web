167a2cb26925baafd3e076906c9d125233f279c1
gabrielbsouzadev <gabrielbsouzadev@gmail.com>
2020-11-23 20:49:37 -0300

    01-Python_3_parte_1-Introducao_a_nova_versao_da_linguagem - finish

--- Alura/04-python/01-Python_3_parte_1-Introducao_a_nova_versao_da_linguagem/jogos/adivinhacao.py
+++ Alura/04-python/01-Python_3_parte_1-Introducao_a_nova_versao_da_linguagem/jogos/adivinhacao.py
@@ -1,81 +1,117 @@
-# print("********************************")
-# print("Comparando a idade do casal")
-# print("********************************")
-# minha_idade = 25
-# idade_namorada = 19
-# if(minha_idade == idade_namorada):
-#     print('temos idades iguais')
-# else:
-#     print('temos idades diferentes')
+def jogar():
 
-
-print("********************************")
-print("bem vindo ao jogo de adivinhacao")
-print("********************************")
-
-numero_secreto = 42
-total_de_tentativas = 3
-rodada = 1
-
-while( True ):
-    print("tentativa {} de {}".format(rodada, total_de_tentativas))
-    chute_str = input("Digite seu numero: ")
-    chute_int = int(chute_str)
-
-    print("Voce digitou ", chute_int)
-
-    if (chute_int < 1 or chute_int > 100) :
+    import random
         
-        print ("Você deve digitar um número entre 1 e 100")
-        continue
+    # print("********************************")
+    # print("Comparando a idade do casal")
+    # print("********************************")
+    # minha_idade = 25
+    # idade_namorada = 19
+    # if(minha_idade == idade_namorada):
+    #     print('temos idades iguais')
+    # else:
+    #     print('temos idades diferentes')
 
-    acertou = numero_secreto == chute_int
-    maior = chute_int > numero_secreto
-    menor = chute_int < numero_secreto
 
-    if (acertou):
-        print("Você acertou")
-        break;
+    print("********************************")
+    print("bem vindo ao jogo de adivinhacao")
+    print("********************************")
+
+    numero_secreto = (random.randrange(1, 101))
+    total_de_tentativas = 3
+    rodada = 1
+
+    # print(numero_secreto)
+
+    while( True ):
+        print("tentativa {} de {}".format(rodada, total_de_tentativas))
+        chute_str = input("Digite seu numero: ")
+        chute_int = int(chute_str)
+
+        print("Voce digitou ", chute_int)
+
+        if (chute_int < 1 or chute_int > 100) :
+            
+            print ("Você deve digitar um número entre 1 e 100")
+            continue
+
+        acertou = numero_secreto == chute_int
+        maior = chute_int > numero_secreto
+        menor = chute_int < numero_secreto
+
+        if (acertou):
+            print("Você acertou")
+            break;
+        else:
+            if (maior):
+                print("Você errou! Seu chute foi maior que o número secreto")
+            #Ou poderia usar o else     
+            elif(menor):
+                print("Você errou! Seu chute foi menor que o número secreto")
+
+        if (rodada == total_de_tentativas):
+            break
+
+        rodada = rodada + 1
+        
+
+    print("Utilizando o FOR")
+
+    import random
+
+    print("*********************************")
+    print("Bem vindo ao jogo de Adivinhação!")
+    print("*********************************")
+
+    numero_secreto = random.randrange(1, 101)
+    total_de_tentativas = 0
+    pontos = 1000
+
+    print("Qual o nível de dificuldade?")
+    print("(1) Fácil (2) Médio (3) Difícil")
+
+    nivel = int(input("Defina o nível: "))
+
+    if(nivel == 1):
+        total_de_tentativas = 20
+    elif(nivel == 2):
+        total_de_tentativas = 10
     else:
-        if (maior):
-            print("Você errou! Seu chute foi maior que o número secreto")
-        #Ou poderia usar o else     
-        elif(menor):
-            print("Você errou! Seu chute foi menor que o número secreto")
+        total_de_tentativas = 5
 
-    if (rodada == total_de_tentativas):
-        break
+    for rodada in range(1, total_de_tentativas + 1):
+        print("Tentativa {} de {}".format(rodada, total_de_tentativas))
 
-    rodada = rodada + 1
-    
+        chute_str = input("Digite um número entre 1 e 100: ")
+        print("Você digitou ", chute_str)
+        chute = int(chute_str)
 
-#Utilizando o FOR
+        if(chute < 1 or chute > 100):
+            print("Você deve digitar um número entre 1 e 100!")
+            continue
 
-numero_secreto = 42
-total_de_tentativas = 3
+        acertou = chute == numero_secreto
+        maior = chute > numero_secreto
+        menor = chute < numero_secreto
 
-for rodada in range(1, total_de_tentativas + 1):
-    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-    chute_str = input("Digite um número de 1 a 100: ")
-    print("Você digitou: ", chute_str)
-    chute = int(chute_str)
+        if(acertou):
+            print("Você acertou e fez {} pontos!".format(pontos))
+            break
+        else:
+            pontos_perdidos = abs(numero_secreto - chute)
+            pontos = pontos - pontos_perdidos
+            if(maior):
+                print("O seu chute foi maior que o número secreto")
+                if (rodada == total_de_tentativas):
+                    print("O número secreto era {}. Você fez {}".format(
+                        numero_secreto, pontos))
+            elif(menor):
+                print("Você errou! O seu chute foi menor do que o número secreto.")
+                if (rodada == total_de_tentativas):
+                    print("O número secreto era {}. Você fez {}".format(
+                        numero_secreto, pontos))
 
-    if (chute < 1 or chute > 100) :
-        rodada = rodada - 1
-        print ("Você deve digitar um número entre 1 e 100")
-        continue
+    print("Fim do jogo")
 
-    acertou = numero_secreto == chute
-    maior = chute > numero_secreto
-    menor = chute < numero_secreto
-
-    if (acertou):
-        print("Você acertou!")
-        break
-    else:
-        if (maior):
-            print("Você errou! O seu chute foi maior que o número secreto.")
-        elif (menor):
-            print("Você errou! O seu chute foi menor que o número secreto.")
-
-print("Fim do jogo")
+if(__name__ == "__main__"):
+    jogar()
