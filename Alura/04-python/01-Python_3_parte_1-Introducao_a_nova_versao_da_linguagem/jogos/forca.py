610081d37383cfc559fc12ef4db87fcd937ea0b6
gabrielbsouzadev <gabrielbsouzadev@gmail.com>
2020-11-24 10:47:28 -0300

    Python 3 parte 2: Avançando na linguagem - Manipulando Strings - Mão na Massa

--- Alura/04-python/01-Python_3_parte_1-Introducao_a_nova_versao_da_linguagem/jogos/forca.py
+++ Alura/04-python/01-Python_3_parte_1-Introducao_a_nova_versao_da_linguagem/jogos/forca.py
@@ -2,6 +2,14 @@ def jogar():
     print("********************************")
     print("***Bem vindo ao jogo de Forca***")
     print("********************************")
+    
+    palavra_secreta = "banana"
+    
+    enforcou = False
+    acertou = False
+    
+    while (not acertou and not enforcou):
+        print("Jogando...")
 
     print("Fim do jogo")
 
